2dd14783065c9927083d99500f5e1bb39b9c4266
Aleksey Pomendyukov <alexei963@yandex.ru>
2022-08-04 16:27:08 +0400

    Change application.yml, MvcConfig.

--- src/main/java/com/example/blog/config/MvcConfig.java
+++ src/main/java/com/example/blog/config/MvcConfig.java
@@ -13,7 +13,7 @@ public class MvcConfig implements WebMvcConfigurer {
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/projects/**")
-            .addResourceLocations("file:/projects/");
+    registry.addResourceHandler("/upload/**")
+            .addResourceLocations("file:/upload/");
   }
 }
